350f0606a03473cc8f4b5ca46fd80814ce7f7a4e
Killian Mancheron <killian.mancheron@dauphine.eu>
2021-08-06 21:39:26 +0200

    Mise à jour

--- Agent/MARL_DQN.py
+++ Agent/MARL_DQN.py
@@ -4,46 +4,22 @@ import numpy as np
 from keras.models import Model
 from collections import deque
 import time
-from .DeepQNetwork import DeepQN
 
 def one_hot(action, k=4):
   return [int(action==i) for i in range(k)]
 
-class MARL_DeepQN(DeepQN):
+class MARL_DeepQN():
 
-    
+  def __init__(self, model, joueur, gamma =.99, batch_size = 32):
+    #model:
+    self.model = model
 
-class AbstractDeepQN():
-
-  def __init__(self, nb_actions, gamma =.99, batch_size = 32):
     #constantes:
-    self.nb_actions = nb_actions
     self.gamma = gamma
     self.batch_size = batch_size
+    self.joueur=joueur
     
     #etat:
-    self.compiled = False
-
-  def get_config(self):
-    return {
-        'nb_actions': self.nb_actions,
-        'gamma': self.gamma,
-        'batch_size': self.batch_size
-    }
-
-class DeepQN(AbstractDeepQN):
-
-  def __init__(self, model, *args, **kwargs):
-    #on hérite des paramètres de la classe abstraite
-    super(DeepQN, self).__init__(*args, **kwargs)
-
-    if model.output.shape.as_list() != [None, self.nb_actions]:
-      raise ValueError("Le model doit avoir un output avec autant d'actions possibles")
-    self.model = model
-    #etat :
-    self.reset_state()
-
-  def reset_state(self):
     self.compiled = False
 
   def update_target_model(self):
@@ -72,41 +48,53 @@ class DeepQN(AbstractDeepQN):
     self.model.compile(optimizer=optimizer, loss='mse')
     self.compiled = True
 
-
+  def one_hot(action):
+    return [int(action==i) for i in range(4)]
 
   def Q_values(self, state, action):
-    state = np.array(state).reshape((-1,state.shape[0],state.shape[1],state.shape[2]))
+    state = np.array(state).reshape((-1,)+state.shape)
     action = np.array(one_hot(action)).reshape(-1,4)
-    return np.array(self.model([state,action])[0])
+    return np.array([max(self.model([state,action])[0].numpy() for action in range(4))])
 
-  def update(self, batch): # reward, done, observation
+  def eval(self, states):
+    values = []
+    for state in states:
+      state =  np.array(state).reshape((-1,)+state.shape)
+      Q_values = np.zeros((4,4))
+      for action in range(4):
+        action_vect = np.array(one_hot(action)).reshape(-1,4)
+        Q_values[action,:] = self.target_model([ state,action_vect ])[0].numpy()
+      index = np.unravel_index(np.argmax(Q_values, axis=None), Q_values.shape)
+      values.append(np.min(Q_values[index[0],:]))
+    return values
+
+  def update(self, batch): 
     if not self.compiled :
       raise ValueError("Compiler l'algorithme avant la mise à jour des poids")
 
     num_batch = len(batch)
-    #copier les poids du model
-    #self.update_target_model()
-    #
+
     states=[]
-    actions=[]
+    actions = []
     observations=[]
     for state, action, reward, observation, done in batch:
       states.append(list(state))
-      actions.append(list(action))
+      actions.append(list(one_hot(a) for a in action))
       observations.append(list(observation))
-    states = np.array(states).reshape(num_batch,observation.shape[0],observation.shape[1],observation.shape[2])
-    actions = np.array(actions).reshape(num_batch,4)
-    observations = np.array(observations).reshape(num_batch,observation.shape[0],observation.shape[1],observation.shape[2])
+    states = np.array(states).reshape((num_batch,)+observation.shape)
+    observations = np.array(observations).reshape((num_batch,)+observation.shape)
+    actions = np.array([actions]).reshape(num_batch,2,4)
 
-    targets = np.array(self.model(states))
-    observation_targets = np.array(self.target_model(observations))
- 
+    opposant = 1 - self.joueur
+    targets = np.array(self.model([states,actions[:,opposant,:]]))
+    observation_targets = np.array(self.eval(observations))
+
     for i, experience in enumerate(batch):
       state, action, reward, observation, done = experience
       target = targets[i]
-      if done :
-        target[action] = reward
+      if done[0] :
+        target[action[0]] = reward[0]
       else :
-        target[action] = reward + max(observation_targets[i]) * self.gamma
+        target[action[0]] = reward[0] + observation_targets[i] *self.gamma
 
-    self.model.fit(states, targets, epochs=1, verbose=0)
+    self.model.fit([states,actions[:,opposant,:]], targets, epochs=1, verbose=0)
