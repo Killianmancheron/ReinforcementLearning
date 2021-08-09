64e3efa16f573aa4fc3e574ded5d45f1c1e19007
Killian Mancheron <killian.mancheron@dauphine.eu>
2021-08-09 10:47:36 +0200

    Mise à jour

--- Environnment/controller.py
+++ Environnment/controller.py
@@ -164,7 +164,8 @@ class Controller(Abstract_Controller):
     # On souhaite autant de pommes que de serpents vivants
     while (len(self.grid.apples)<self.nb_apples):
       new_apple = self.grid.spawn_apple()
-      self.h = self.h ^ self.hashTable [self.grid.APPLE_CODE] [new_apple[0]] [new_apple[1]]
+      if new_apple:
+        self.h = self.h ^ self.hashTable [self.grid.APPLE_CODE] [new_apple[0]] [new_apple[1]]
     
     self.h = self.h ^ self.hashTurn
     if self.nb_snakes!=1:
